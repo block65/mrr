1392d8127703260ca4454b115ed5e515c4ba8030
Max Holman <max@holmn.com>
2022-09-21 15:39:58 +0800

    chore: lint

--- __tests__/route.test.tsx
+++ __tests__/route.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import type { ComponentProps, FC, PropsWithChildren } from 'react';
+import type { ComponentProps, FC } from 'react';
 import { namedRoute } from '../lib/named-route.js';
 import { Route, Router, Routes } from '../src/index.js';
 
