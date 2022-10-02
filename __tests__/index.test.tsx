7f3b972ce0cdb600531716a454a9b3b8593e38c9
Max Holman <max@holmn.com>
2022-10-02 18:28:45 +0800

    chore: move nested tests out into separate file

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,7 @@
 import '@testing-library/jest-dom';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import type { FC } from 'react';
-import {
-  Link,
-  Route,
-  RouteComponentProps,
-  Router,
-  Routes,
-  useLocation,
-} from '../src/index.js';
+import { Link, Route, Router, Routes, useLocation } from '../src/index.js';
 import { namedRoute } from '../lib/named-route.js';
 import { useNavigate } from '../lib/router.js';
 
@@ -54,50 +47,6 @@ test('basic', async () => {
   expect(screen.getByRole('heading')).toHaveTextContent(
     'You are at the login page',
   );
-
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test('wildcard routes + nested', async () => {
-  const userRoot = namedRoute('/users');
-  const userView = namedRoute('/users/blah/:userId');
-
-  const ComponentWithUserId: FC<RouteComponentProps<{ userId: string }>> = ({
-    params: { userId },
-  }) => <>userId = {userId}</>;
-
-  const ParamlessComponent: FC = () => <>I am a Paramless Component</>;
-
-  const { asFragment } = render(
-    <Router pathname="/users/blah/test1">
-      <LocationDisplay />
-      <Routes>
-        <Route wildcard path={userRoot.path}>
-          <h1>inside userRoot</h1>
-          <Routes>
-            <Route path={userRoot.path}>this should not display</Route>
-            <Route wildcard path={userView.path}>
-              {(params) => (
-                <h1 data-testid="users">You are user {params.userId}</h1>
-              )}
-            </Route>
-            <Route path={userView.path} component={ComponentWithUserId} />
-            <Route component={ParamlessComponent} />
-            <Route>{() => <ParamlessComponent />}</Route>
-            <Route>
-              <h1>fail</h1>
-            </Route>
-          </Routes>
-        </Route>
-
-        <Route>
-          <h1>fail</h1>
-        </Route>
-      </Routes>
-    </Router>,
-  );
-
-  await waitFor(() => screen.getByTestId('users'));
 
   expect(asFragment()).toMatchSnapshot();
 });
---
+++ __tests__/nested.test.tsx
@@ -0,0 +1,85 @@
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import type { FC } from 'react';
+import { namedRoute } from '../lib/named-route.js';
+import { Route, RouteComponentProps, Router, Routes } from '../src/index.js';
+import { LocationDisplay } from './index.test.js';
+
+test('wildcard routes + nested', async () => {
+  const userRoot = namedRoute('/users');
+  const userView = namedRoute('/users/blah/:userId');
+
+  const ComponentWithUserId: FC<RouteComponentProps<{ userId: string }>> = ({
+    params: { userId },
+  }) => <>userId = {userId}</>;
+
+  const ParamlessComponent: FC = () => <>I am a Paramless Component</>;
+
+  const { asFragment } = render(
+    <Router pathname="/users/blah/test1">
+      <LocationDisplay />
+      <Routes>
+        <Route wildcard path={userRoot.path}>
+          <h1>inside userRoot</h1>
+          <Routes>
+            <Route path={userRoot.path}>this should not display</Route>
+            <Route wildcard path={userView.path}>
+              {(params) => (
+                <h1 data-testid="users">You are user {params.userId}</h1>
+              )}
+            </Route>
+            <Route path={userView.path} component={ComponentWithUserId} />
+            <Route component={ParamlessComponent} />
+            <Route>{() => <ParamlessComponent />}</Route>
+            <Route>
+              <h1>fail</h1>
+            </Route>
+          </Routes>
+        </Route>
+
+        <Route>
+          <h1>fail</h1>
+        </Route>
+      </Routes>
+    </Router>,
+  );
+
+  await waitFor(() => screen.getByTestId('users'));
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('default route + wildcard routes + nested', async () => {
+  const userView = namedRoute('/users/blah/:userId');
+
+  const { asFragment } = render(
+    <Router pathname="/users/blah/test1">
+      <LocationDisplay />
+      <Routes>
+        <Route>
+          <Routes>
+            <Route>
+              <Routes>
+                <Route wildcard path={userView.path}>
+                  {(params) => (
+                    <h1 data-testid="users">You are user {params.userId}</h1>
+                  )}
+                </Route>
+                <Route>
+                  <h1>fail</h1>
+                </Route>
+              </Routes>
+            </Route>
+            <Route>
+              <h1>fail</h1>
+            </Route>
+          </Routes>
+        </Route>
+      </Routes>
+    </Router>,
+  );
+
+  await waitFor(() => screen.getByTestId('users'));
+
+  expect(asFragment()).toMatchSnapshot();
+});
