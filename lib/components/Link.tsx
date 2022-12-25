2a5b649028a1d540a242db8c2309d486f8bde07b
Max Holman <max@holmn.com>
2022-12-25 19:58:31 +0800

    refactor split components into separate files

--- lib/components/Link.tsx
+++ lib/components/Link.tsx
@@ -1,7 +1,6 @@
 import {
   AnchorHTMLAttributes,
   cloneElement,
-  FC,
   ForwardedRef,
   forwardRef,
   isValidElement,
@@ -9,44 +8,14 @@ import {
   MouseEvent,
   PropsWithChildren,
   useCallback,
-  useLayoutEffect,
 } from 'react';
 import {
   Destination,
   NavigationMethodOptions,
   useLocation,
   useRouter,
-} from './router.js';
-import { useMatch } from './routes.js';
-import type {
-  DefaultRouteProps,
-  ExtractRouteParams,
-  Params,
-  RouteProps,
-} from './types.js';
-import { calculateDest, nullOrigin, urlRhs } from './util.js';
-
-export const Route = <
-  TPath extends string,
-  TProps extends Params = ExtractRouteParams<TPath>,
->(
-  props:
-    | DefaultRouteProps
-    | PropsWithChildren<RouteProps<TPath> & { component?: never }>
-    | (RouteProps<TPath> & {
-        component: FC<PropsWithChildren<TProps>>;
-        children?: never;
-      }),
-): ReturnType<FC<typeof props>> => {
-  const match = useMatch<TProps>();
-
-  if (props && 'component' in props && typeof props.component === 'function') {
-    return props.component(match ? match.params : ({} as TProps));
-  }
-
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{props.children}</>;
-};
+} from '../router.js';
+import { calculateDest, nullOrigin, urlRhs } from '../util.js';
 
 export const Link = forwardRef<
   HTMLAnchorElement,
@@ -118,25 +87,9 @@ export const Link = forwardRef<
   // its not possible to tell if a child will accept a href prop or not
   // we can only tell if its a component or not
   // so we simply always wrap in an anchor, unless it's already an anchor
-  return isValidElement(children) && children.type === 'a' ? (
+  return isValidElement(children) ? (
     cloneElement(children, newProps)
   ) : (
     <a {...newProps}>{children}</a>
   );
 });
-
-export const Redirect: FC<
-  PropsWithChildren<
-    NavigationMethodOptions & {
-      dest: Destination;
-    }
-  >
-> = ({ dest, children, history }) => {
-  const [, { navigate }] = useLocation();
-
-  useLayoutEffect(() => {
-    navigate(dest, history && { history });
-  }, [dest, history, navigate]);
-
-  return <>{children}</>;
-};
