5061d9e630c3cec99582d7b7975980ea7b825d04
Max Holman <max@holmn.com>
2022-12-25 20:01:29 +0800

    refactor: avoid dep cycle

--- lib/routes.tsx
+++ lib/routes.tsx
@@ -7,7 +7,6 @@ import {
   ReactElement,
   useContext,
 } from 'react';
-import { Route } from './components.js';
 import type { Match, MatchResult } from './matcher.js';
 import { useLocation, useRouter } from './router.js';
 import type { Params, RouteComponentProps } from './types.js';
@@ -17,6 +16,22 @@ export type RouteComponent = ReactElement<ComponentProps<typeof Route>>;
 
 export const RoutesContext = createContext<Match>(false);
 
+export function useMatch<TPath extends string>(): Match<TPath> {
+  return useContext(RoutesContext) as Match<TPath>;
+}
+
+export const Route = <TPath extends string>(
+  props: RouteComponentProps<TPath>,
+): ReturnType<FC<typeof props>> => {
+  const match = useMatch<TPath>();
+
+  if (props && 'component' in props && typeof props.component === 'function') {
+    return match ? props.component(match.params) : null;
+  }
+
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <>{props.children}</>;
+};
 
 export const Routes: FC<PropsWithChildren> = ({ children }) => {
   const [url] = useLocation();
@@ -47,7 +62,3 @@ export const Routes: FC<PropsWithChildren> = ({ children }) => {
     <RoutesContext.Provider value={matchResult}>{child}</RoutesContext.Provider>
   );
 };
-
-export function useMatch<T extends Params>(): Match<T> {
-  return useContext(RoutesContext) as Match<T>;
-}
