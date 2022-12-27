870f76354a8743ca703ed9d38da2aa247bb77a2d
Max Holman <max@holmn.com>
2022-12-27 17:40:20 +0800

    chore: add tests for route component/children + effects in those components

--- __tests__/route.test.tsx
+++ __tests__/route.test.tsx
@@ -1,15 +1,17 @@
 import '@testing-library/jest-dom';
+import { jest } from '@jest/globals';
 import { render } from '@testing-library/react';
-import type { ComponentProps, FC } from 'react';
+import type { FC, PropsWithChildren } from 'react';
+import { useEffect } from 'react';
 import { namedRoute } from '../lib/named-route.js';
 import { Route, Router, Routes } from '../src/index.js';
 
-const CustomRoute: FC<ComponentProps<typeof Route>> = (props) => (
-  <Route {...props} />
-);
+const login = namedRoute('/');
 
 test('custom route', async () => {
-  const login = namedRoute('/');
+  const CustomRoute = (props: PropsWithChildren<{ path: string }>) => (
+    <Route {...props} />
+  );
 
   const { asFragment } = render(
     <Router>
@@ -22,4 +24,49 @@ test('custom route', async () => {
   );
 
   expect(asFragment()).toMatchSnapshot();
+});
+
+test('routes with components/children/paths/no paths', async () => {
+  const HelloComponent: FC = () => <h1>hello</h1>;
+
+  const { asFragment } = render(
+    <Router>
+      <Routes>
+        <Route path={login.path} component={HelloComponent} />
+        <Route path={login.path}>
+          <HelloComponent />
+        </Route>
+        <Route component={HelloComponent} />
+        <Route>
+          <HelloComponent />
+        </Route>
+      </Routes>
+    </Router>,
+  );
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('effects inside route components or children dont fire', async () => {
+  const effectFn = jest.fn();
+
+  const HelloComponent: FC = () => {
+    useEffect(() => {
+      effectFn();
+    }, []);
+    return <h1>hello</h1>;
+  };
+
+  render(
+    <Router>
+      <Routes>
+        <Route path={'/random'} component={HelloComponent} />
+        <Route path={'random2'}>
+          <HelloComponent />
+        </Route>
+      </Routes>
+    </Router>,
+  );
+
+  expect(effectFn).not.toHaveBeenCalled();
 });
