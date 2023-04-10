1357fa8f7bec6f19f380108f2ee9ea20ac8dff6b
Max Holman <max@holmn.com>
2023-04-10 16:35:46 +0800

    chore: update test snapshots as required

--- __tests__/named-route.test.ts
+++ __tests__/named-route.test.ts
@@ -7,32 +7,32 @@ const route3 = namedRoute('/test/:welp');
 const route4 = namedRoute('/test/:welp/:welp2/:kelp3?');
 
 test('custom route', async () => {
-  expect(route1.build()).toMatchInlineSnapshot(`"http://localhost/"`);
-  expect(route2.build()).toMatchInlineSnapshot(`"http://localhost/test"`);
+  expect(route1.build()).toMatchInlineSnapshot(`"/"`);
+  expect(route2.build()).toMatchInlineSnapshot(`"/test"`);
   expect(
     route3.build({
       params: {
         welp: '123',
       },
     }),
-  ).toMatchInlineSnapshot(`"http://localhost/test/123"`);
+  ).toMatchInlineSnapshot(`"/test/123"`);
 
   expect(
     route3.build({
       // @ts-expect-error - required params not supplied
       params: {},
     }),
-  ).toMatchInlineSnapshot(`"http://localhost/test"`);
+  ).toMatchInlineSnapshot(`"/test"`);
 
   expect(
     // @ts-expect-error - required prop not supplied
     route3.build({}),
-  ).toMatchInlineSnapshot(`"http://localhost/test/:welp"`);
+  ).toMatchInlineSnapshot(`"/test/:welp"`);
 
   expect(
     // @ts-expect-error - required arg not supplied
     route3.build(),
-  ).toMatchInlineSnapshot(`"http://localhost/test/:welp"`);
+  ).toMatchInlineSnapshot(`"/test/:welp"`);
 
   expect(
     route4.build({
@@ -41,5 +41,15 @@ test('custom route', async () => {
         welp2: '456',
       },
     }),
-  ).toMatchInlineSnapshot(`"http://localhost/test/123/456"`);
+  ).toMatchInlineSnapshot(`"/test/123/456"`);
+
+  expect(
+    route4.build({
+      params: {
+        welp: '123',
+        welp2: '456',
+      },
+      origin: 'https://example.com',
+    }),
+  ).toMatchInlineSnapshot(`"https://example.com/test/123/456"`);
 });
