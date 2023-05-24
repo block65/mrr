ed5c4e6b10ae2e7cf78d04f965f77bd546bfb69d
Max Holman <max@holmn.com>
2023-05-24 16:35:26 +0800

    feat: export the route match hook

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 export { Redirect } from '../lib/components/Redirect.js';
 export { Link, type LinkProps } from '../lib/components/Link.js';
+
 export { Router, type PartialNavigateEventListener } from '../lib/router.js';
-export { useLocation, useRouter } from '../lib/use-router.js';
+export {
+  useLocation,
+  useRouter,
+  useHook as useRouterHook,
+} from '../lib/use-router.js';
+
 export { Routes, Route } from '../lib/routes.js';
+export { useRouteMatch } from '../lib/use-route-match.js';
 
 export * from '../lib/hooks.js';
 
