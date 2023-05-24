127c73fddfb7ea4f0488c22c6a5ee9ae2e1b700c
Max Holman <max@holmn.com>
2023-05-24 16:27:17 +0800

    chore: renamed file for DX

--- lib/use-route-match.tsx
+++ lib/use-route-match.tsx
@@ -2,6 +2,6 @@ import { useContext } from 'react';
 import { RoutesContext } from './RoutesContext.js';
 import type { Match } from './matcher.js';
 
-export function useMatch<TPath extends string>(): Match<TPath> {
+export function useRouteMatch<TPath extends string>(): Match<TPath> {
   return useContext(RoutesContext) as Match<TPath>;
 }
